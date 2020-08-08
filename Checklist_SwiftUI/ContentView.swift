106463fd78ff3dddf6246e761e761afa5fa85b91
David Fejes <davidfejes.dev@gmail.com>
2020-08-08 09:53:20 +0200

    Initial Commit

---
+++ Checklist_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Checklist_SwiftUI
+//
+//  Created by Dávid Fejes on 2020. 08. 08..
+//  Copyright © 2020. David Fejes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
