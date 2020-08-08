38e7b9648c25bf16cf2d6246e2b8b6e0d3bcc422
David Fejes <davidfejes.dev@gmail.com>
2020-08-08 12:19:26 +0200

    to-do app list view

--- Checklist_SwiftUI/ContentView.swift
+++ Checklist_SwiftUI/ContentView.swift
@@ -9,9 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+  
+  @State var checklistItems = [
+    "Walk the dog",
+    "Brush my teeth",
+    "Learn iOS development",
+    "Soccer practice",
+    "Eat ice cream",
+  ]
+
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(checklistItems, id: \.self) { item in
+          Text(item)
+        }
+        .onDelete(perform: deleteListItem)
+        .onMove(perform: moveListItem)
+      }
+      .navigationBarItems(trailing: EditButton())
+      .navigationBarTitle("Checklist")
     }
+  }
+  
+  func deleteListItem(whichElement: IndexSet) {
+    checklistItems.remove(atOffsets: whichElement)
+  }
+  
+  func moveListItem(whichElement: IndexSet, destination: Int) {
+    checklistItems.move(fromOffsets: whichElement, toOffset: destination)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
