5942f7b88b2b4f6fb3ce849b6e4635610228d232
David Fejes <davidfejes.dev@gmail.com>
2020-08-09 12:31:07 +0200

    struct updates & onTapGesture methods

--- Checklist_SwiftUI/ContentView.swift
+++ Checklist_SwiftUI/ContentView.swift
@@ -8,38 +8,67 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  
-  @State var checklistItems = [
-    "Walk the dog",
-    "Brush my teeth",
-    "Learn iOS development",
-    "Soccer practice",
-    "Eat ice cream",
-  ]
-
-  var body: some View {
-    NavigationView {
-      List {
-        ForEach(checklistItems, id: \.self) { item in
-          Text(item)
-        }
-        .onDelete(perform: deleteListItem)
-        .onMove(perform: moveListItem)
-      }
-      .navigationBarItems(trailing: EditButton())
-      .navigationBarTitle("Checklist")
-    }
-  }
-  
-  func deleteListItem(whichElement: IndexSet) {
-    checklistItems.remove(atOffsets: whichElement)
-  }
-  
-  func moveListItem(whichElement: IndexSet, destination: Int) {
-    checklistItems.move(fromOffsets: whichElement, toOffset: destination)
-  }
+struct ChecklistItem: Identifiable {
+    let id = UUID() // 'UUID' is a built-in struct which generates a universally unique value every time it's called.
+    var name: String
+    var isChecked: Bool = false
 }
+
+struct ContentView: View {
+    
+    // Properties
+    // ==========
+    
+    @State var checklistItems = [
+        ChecklistItem(name: "Walk the dog", isChecked: false),
+        ChecklistItem(name: "Brush my teeth", isChecked: false),
+        ChecklistItem(name: "Learn iOS development", isChecked: true),
+        ChecklistItem(name: "Soccer practice", isChecked: false),
+        ChecklistItem(name: "Eat ice cream", isChecked: true),
+    ]
+    
+    // User interface content and layout
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(checklistItems) { checklistItem in
+                    HStack {
+                        Text(checklistItem.name)
+                        Spacer()
+                        Text(checklistItem.isChecked ? "✅" : "🔲")
+                    }
+                        .background(Color(UIColor.systemBackground)) // This makes the entire row clickable
+                        .onTapGesture {
+                            if let matchingIndex = self.checklistItems.firstIndex(where: { $0.id == checklistItem.id }) {
+                                self.checklistItems[matchingIndex].isChecked.toggle()
+                            }
+                    }
+                }
+                .onDelete(perform: deleteListItem)
+                .onMove(perform: moveListItem)
+            }
+            .navigationBarItems(trailing: EditButton())
+            .navigationBarTitle("Checklist")
+        }
+    }
+    
+    
+    // Methods
+    // =======
+    
+    func deleteListItem(whichElement: IndexSet) {
+        checklistItems.remove(atOffsets: whichElement)
+    }
+    
+    func moveListItem(whichElement: IndexSet, destination: Int) {
+        checklistItems.move(fromOffsets: whichElement, toOffset: destination)
+    }
+    
+}
+
+
+// Preview
+// =======
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
